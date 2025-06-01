6f742eefd97697479300b492a9ce0c967b864100
Patrick <patrickbustamante61@gmail.com>
2025-05-31 19:59:49 -0600

    Added basic product slider

--- JerryList/src/routes/index.svelte
+++ JerryList/src/routes/index.svelte
@@ -1,31 +1,38 @@
-<body>
-	<nav>
-		<div class="navTop">
-			<div class="navItem">
-				<img src="/favicon.png" alt="alt test" />
-			</div>
-			<div class="navItem">
-				<div class="search">
-					<input type="text" placeholder="Search..." class="searchInput" />
-					<img src="/searchicon.png" width="20" height="20" alt="" class="searchIcon" />
-				</div>
-			</div>
-			<div class="navItem">
-				<span class="limitedOffer"> Limited Offer! </span>
+<script>
+	import Slider from './slider.svelte';
+	import { currentIndex } from './slideractions';
+</script>
+
+<nav>
+	<div class="navTop">
+		<!-- <div class="navItem"> -->
+		<!-- 	<img src="/favicon.png" alt="alt test" /> -->
+		<!-- </div> -->
+		<div class="navItem">
+			<div class="search">
+				<input type="text" placeholder="Search..." class="searchInput" />
+				<img src="/searchicon.png" width="20" height="20" alt="" class="searchIcon" />
 			</div>
 		</div>
-		<div class="navBottom">
-			<h3 class="menuItem">AIR FORCE</h3>
-			<h3 class="menuItem">JORDAN</h3>
-			<h3 class="menuItem">BLAZER</h3>
-			<h3 class="menuItem">CRATER</h3>
-			<h3 class="menuItem">HIPPIE</h3>
-		</div>
-	</nav>
-</body>
+		<!-- <div class="navItem"> -->
+		<!-- 	<span class="limitedOffer"> Limited Offer! </span> -->
+		<!-- </div> -->
+	</div>
+	<div class="navBottom">
+		{#each Array(5) as _, i}
+			<h3 class="menuItem" onclick={() => ($currentIndex = i)}>Product {i + 1}</h3>
+			<!-- <h3 class="menuItem">Product2</h3> -->
+			<!-- <h3 class="menuItem">Product3</h3> -->
+			<!-- <h3 class="menuItem">Product4</h3> -->
+			<!-- <h3 class="menuItem">Product5</h3> -->
+		{/each}
+	</div>
+</nav>
+
+<Slider />
 
 <style>
-	body {
+	:global(body) {
 		font-family: sans-serif;
 		padding: 0;
 		margin: 0;
@@ -40,7 +47,7 @@
 	.navTop {
 		display: flex;
 		align-items: center;
-		justify-content: space-between;
+		justify-content: center;
 	}
 
 	.search {
@@ -60,16 +67,11 @@
 		color: lightgrey;
 	}
 
-	.limitedOffer {
-		font-size: 20px;
-		border-bottom: 2px solid green;
-		cursor: pointer;
-	}
-
 	.navBottom {
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		margin-top: 10px;
 	}
 
 	.menuItem {
---
+++ JerryList/src/routes/slider.svelte
@@ -0,0 +1,148 @@
+<script>
+	import { currentIndex } from './slideractions';
+</script>
+
+<div class="slider">
+	<div class="sliderWrapper" style="transform: translateX({-100 * $currentIndex}vw)">
+		<div class="sliderItem">
+			<img src="/Prod1.png" alt="Components" class="sliderImg" />
+			<div class="slideBg"></div>
+			<h1 class="sliderTitle">PC <br /> Components</h1>
+			<h2 class="sliderPrice">$Price</h2>
+			<button class="buyButton">Buy now</button>
+		</div>
+		<div class="sliderItem">
+			<img src="/Prod2.png" alt="Components" class="sliderImg" />
+			<div class="slideBg"></div>
+			<h1 class="sliderTitle">PC <br /> Components</h1>
+			<h2 class="sliderPrice">$Price</h2>
+			<button class="buyButton">Buy now</button>
+		</div>
+		<div class="sliderItem">
+			<img src="/Prod3.png" alt="Components" class="sliderImg" />
+			<div class="slideBg"></div>
+			<h1 class="sliderTitle">PC <br /> Components</h1>
+			<h2 class="sliderPrice">$Price</h2>
+			<button class="buyButton">Buy now</button>
+		</div>
+		<div class="sliderItem">
+			<img src="/Prod4.png" alt="Components" class="sliderImg" />
+			<div class="slideBg"></div>
+			<h1 class="sliderTitle">PC <br /> Components</h1>
+			<h2 class="sliderPrice">$Price</h2>
+			<button class="buyButton">Buy now</button>
+		</div>
+		<div class="sliderItem">
+			<img src="/Prod5.png" alt="Components" class="sliderImg" />
+			<div class="slideBg"></div>
+			<h1 class="sliderTitle">PC <br /> Components</h1>
+			<h2 class="sliderPrice">$Price</h2>
+			<button class="buyButton">Buy now</button>
+		</div>
+	</div>
+</div>
+
+<style>
+	.slider {
+		background-color: #e5e5f7;
+		opacity: 0.8;
+		background-size: 20px 20px;
+		background-image: repeating-linear-gradient(0deg, #444cf7, #444cf7 1px, #e5e5f7 1px, #e5e5f7);
+		clip-path: polygon(0 0, 100% 0, 100% 50%, 100% 100%, 45% 100%, 0% 50%);
+	}
+
+	.sliderWrapper {
+		display: flex;
+		width: 500vw;
+	}
+
+	.sliderItem {
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+	}
+
+	.slideBg {
+		width: 750px;
+		height: 750px;
+		border-radius: 50%;
+		position: absolute;
+	}
+
+	.sliderImg {
+		z-index: 1;
+	}
+
+	.sliderTitle {
+		position: absolute;
+		top: 10%;
+		right: 10%;
+		font-size: 60px;
+		font-weight: 900;
+		text-align: center;
+		z-index: 1;
+	}
+
+	.sliderPrice {
+		position: absolute;
+		top: 10%;
+		left: 10%;
+		font-size: 60px;
+		font-weight: 300;
+		text-align: center;
+		border: 1px solid grey;
+		z-index: 1;
+	}
+
+	.buyButton {
+		position: absolute;
+		top: 50%;
+		right: 10%;
+		font-size: 30px;
+		font-weight: 900;
+		border: 1px solid grey;
+		background-color: black;
+		color: white;
+		z-index: 1;
+		cursor: pointer;
+	}
+
+	.buyButton:hover {
+		background-color: white;
+		color: black;
+	}
+
+	.sliderItem:nth-child(1) .slideBg {
+		background-color: green;
+	}
+	.sliderItem:nth-child(2) .slideBg {
+		background-color: rebeccapurple;
+	}
+	.sliderItem:nth-child(3) .slideBg {
+		background-color: teal;
+	}
+	.sliderItem:nth-child(4) .slideBg {
+		background-color: cornflowerblue;
+	}
+	.sliderItem:nth-child(5) .slideBg {
+		background-color: rgb(124, 115, 80);
+	}
+
+	.sliderItem:nth-child(1) .sliderPrice {
+		color: green;
+	}
+	.sliderItem:nth-child(2) .sliderPrice {
+		color: rebeccapurple;
+	}
+	.sliderItem:nth-child(3) .sliderPrice {
+		color: teal;
+	}
+	.sliderItem:nth-child(4) .sliderPrice {
+		color: cornflowerblue;
+	}
+	.sliderItem:nth-child(5) .sliderPrice {
+		color: rgb(124, 115, 80);
+	}
+</style>
